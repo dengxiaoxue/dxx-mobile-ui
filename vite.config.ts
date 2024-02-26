270d25147adbc3c1ff4ed5371c9688c2cd391027
dengxiaoxue <dengxx@chn-das.com>
2024-02-26 09:57:56 +0800

    支持外部引入类型

--- vite.config.ts
+++ vite.config.ts
@@ -12,16 +12,22 @@ const vueSfcImportType = (): Plugin => {
     name: "vue-sfc-import-type",
     enforce: "pre",
     async transform(code, id) {
+      // 不是vue文件 返回
       if (!/\.(vue)$/.test(id)) return;
       const typePath = path.resolve(id, "../type.sfc.ts");
+      // 不存在type.sfc.ts文件 返回
       if (!fs.existsSync(typePath)) return;
+      // 文件内容
       let typeFileContent = decomment(fs.readFileSync(typePath).toString());
       typeFileContent = await prettier.format(typeFileContent, {
         semi: true,
         parser: "typescript",
       });
+      // 将文件内容去掉换行符 变成一行
       typeFileContent = typeFileContent.replace(/[\r\n]/g, "");
-      const regex = /import .* from '\.\/type\.sfc'/g;
+      // 替换的正则表达式
+      const regex = /import .* from "\.\/type\.sfc"/g;
+      // 将导入type.sfc的那一行进行替换
       const res = code.replace(regex, typeFileContent);
       return res;
     },
